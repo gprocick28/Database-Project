647c558b2d0cbe43e9392cfc884eec9f17e22f4e
bjennin4 <BJennin4@gmail.com>
2024-05-05 20:11:22 -0400

    Car insertion implemented
    Cars can be added to the database now. Also cleaned up a bit of the HTML and deleted an unused file.

---
+++ frontend/insert.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Insert Cars</title>
+</head>
+
+<body>
+
+<?php // Inserts cars into the database
+
+    include "/users/kent/student/bjennin4/config.php";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Taking values from the form data
+    $first_name =  $_REQUEST['first_name'];
+    $last_name = $_REQUEST['last_name'];
+    $gender =  $_REQUEST['gender'];
+    $address = $_REQUEST['address'];
+    $email = $_REQUEST['email'];
+
+
+    $VIN = $_REQUEST['VIN'];
+    $make = $_REQUEST['make'];
+    $model = $_REQUEST['model'];
+    $year = $_REQUEST['year'];
+    $color = $_REQUEST['color'];
+    $mileage = $_REQUEST['mileage'];
+    $customer_ID = 'NULL';
+
+    $sql = "INSERT INTO `vehicles` VALUES ('" 
+        . $VIN . 
+        "', '" . $make . 
+        "', '" . $model . 
+        "', '" . $year . 
+        "', '" . $color . 
+        "', '" . $mileage . 
+        "', " . $customer_ID . ")";
+
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Car added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+?>
+
+</body>
+</html>
